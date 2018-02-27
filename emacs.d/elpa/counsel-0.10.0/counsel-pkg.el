c957351e86e5726a4e3908d2f3e9aff3008024f6
Patrick Dubroy <pdubroy@lyft.com>
2018-02-27 10:34:26 +0100

    Lots of updates to Emacs config

---
+++ emacs.d/elpa/counsel-0.10.0/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "0.10.0" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "4a2cee03519f98cf95b29905dec2566a39ff717e" :url "https://github.com/abo-abo/swiper" :keywords '("completion" "matching"))
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -9,9 +9,9 @@
 (menu-bar-mode -1)
 
 (setq-default indent-tabs-mode nil
-              tab-width 2
-              c-basic-offset 2
-              js-indent-level 2)
+              tab-width 4
+              c-basic-offset 4
+              js-indent-level 4)
 
 ;; Newline at end of file
 (setq require-final-newline t)
@@ -40,6 +40,13 @@
   (local-set-key  (kbd "C-c o") 'ff-find-other-file))
 (add-hook 'c-mode-common-hook 'my-c-mode-hook)
 
+(global-set-key "\M- " 'hippie-expand)
+
+(defun back-to-indentation-or-beginning () (interactive)
+   (if (= (point) (progn (back-to-indentation) (point)))
+       (beginning-of-line)))
+(global-set-key (kbd "C-a") 'back-to-indentation-or-beginning)
+
 ;; MELPA
 ;; Setup code based on  https://melpa.org/#/getting-started
 (require 'package)
@@ -63,7 +70,9 @@
  '(custom-safe-themes
    (quote
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
- '(package-selected-packages (quote (base16-theme typescript-mode use-package))))
+ '(package-selected-packages
+   (quote
+    (flycheck magit counsel ivy base16-theme typescript-mode use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -83,6 +92,38 @@
   :config
   (load-theme 'base16-default-dark t))
 
+(use-package ivy
+  :ensure t
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  ;;(global-set-key "\C-s" 'swiper)
+  (global-set-key (kbd "C-c C-r") 'ivy-resume)
+  (global-set-key (kbd "<f6>") 'ivy-resume))
+
+(use-package counsel
+  :ensure t
+  :config
+  (global-set-key (kbd "M-x") 'counsel-M-x)
+  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+  (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+  (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+  (global-set-key (kbd "<f1> l") 'counsel-find-library)
+  (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+  (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+  (global-set-key (kbd "C-c g") 'counsel-git)
+  (global-set-key (kbd "C-c j") 'counsel-git-grep)
+  (global-set-key (kbd "C-c k") 'counsel-ag)
+  (global-set-key (kbd "C-x l") 'counsel-locate)
+  (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+  (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history))
+
+(use-package magit
+  :ensure t
+  :config
+  (global-set-key (kbd "C-x g") 'magit-status))
+
 (setq column-number-mode t)
 
 (defun simple-mode-line-render (left right)
@@ -96,3 +137,24 @@
                         (format-mode-line "%* %b (%m)")
                         ;; right
                         (format-mode-line "%l:%c  ")))))
+
+(global-set-key (kbd "C-c b") 'recompile)
+
+;; For programs like rollup, etc. that output color to the terminal
+(ignore-errors
+  (require 'ansi-color)
+  (defun my-colorize-compilation-buffer ()
+    (when (eq major-mode 'compilation-mode)
+      (ansi-color-apply-on-region compilation-filter-start (point-max))))
+  (add-hook 'compilation-filter-hook 'my-colorize-compilation-buffer))
+
+;; Helper for compilation. Close the compilation window if there was no error.
+(defun compilation-exit-autoclose (status code msg)
+  (when (and (eq status 'exit) (zerop code))
+    (bury-buffer)
+    (delete-window (get-buffer-window (get-buffer "*compilation*"))))
+  ;; Always return the anticipated result of compilation-exit-message-function
+  (cons msg code))
+
+;; TODO(pdubroy): Use a lambda here.
+(setq compilation-exit-message-function 'compilation-exit-autoclose)
